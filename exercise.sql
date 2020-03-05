87327ad3a18ba2f918c5b040f8013e2d520d4b70
Candace <2100978@talnet.nl>
2020-03-05 14:41:29 +0100

    Uitwerking van de exercise

--- exercise.sql
+++ exercise.sql
@@ -1,6 +1,6 @@
 
 -- TRUNCATE planeten;
-SELECT * FROM Zonnestelsel;
+SELECT * FROM planets;
   
 CREATE TABLE Zonnestelsel(
 	naam varchar(20),
@@ -11,10 +11,24 @@ CREATE TABLE Zonnestelsel(
 );
 
 INSERT INTO zonnestelsel VALUES(
-	"Neptunus","49572","4498252900","17"
+	"Neptunus","49572","4498252900","17",""
 );
 
+UPDATE zonnestelsel
+SET bezoek_datum = "tess";
 
-ALTER TABLE zonnestelsel ADD naam varchar(20);
 
-DROP TAble zonnestelsel;
+
+
+ALTER TABLE zonnestelsel ADD primary key (naam,diameter,afstand_tot_de_zon,massa_tov_de_aarde);
+DESC zonnestelsel;
+ALTER TABLE zonnestelsel ADD (bezoek_datum datetime);
+INSERT INTO bezoek_datum VALUES('1969-07-20');
+ALTER TABLE zonnestelsel ADD (bezoek_datum  date) ;
+ALTER TABLE zonnestelsel Drop column bezoek_datum;
+
+UPDATE `sterrenstelsel`.`Zonnestelsel` SET `bezoek_datum` = '1969-07-20' WHERE (`naam` = 'Aarde');
+
+
+
+ALTER TABLE planets rename to planeten;
